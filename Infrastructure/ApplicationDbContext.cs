e668306a0f6fef9de61d50c92589186b0bb4f005
drlr21.dl@gmail.com <drlr21.dl@gmail.com>
2025-05-23 07:12:37 -0500

    modelos categorias y gastos

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -4,10 +4,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) // Llama al constructor de la clase base
+        { }
         //adicionar aqui las entidades que se van a usar en la aplicacion
         //public DbSet<Milk> Milks { get; set; }
         public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<Gastos> Gastos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Configuración de la relación entre Gastos y Categorias
+            builder.Entity<Gastos>()
+                .HasOne(g => g.Categoria)
+                .WithMany(c => c.Gastos)
+                .HasForeignKey(g => g.CategoriaId)
+                .OnDelete(DeleteBehavior.Cascade); // Configura la eliminación en cascada si es necesario;
+                
+        }
     }
 }
